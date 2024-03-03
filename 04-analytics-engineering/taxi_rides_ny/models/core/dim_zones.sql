adfda10306f83bd1e02923dc1b9dc4b3337eb50e
Bastien Winant <bastien.winant@gmail.com>
2024-03-03 21:15:23 +0000

    create a model for merging green & yellow trips into a facts table

--- 04-analytics-engineering/taxi_rides_ny/models/core/dim_zones.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/dim_zones.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='table') }}
 
-select 
-    locationid, 
-    borough, 
-    zone, 
+select
+    locationid as location_id,
+    borough,
+    zone,
     replace(service_zone,'Boro','Green') as service_zone 
 from {{ ref('taxi_zone_lookup') }}
